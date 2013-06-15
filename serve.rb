8054ad7010e990594a20f75389cc1fcff47bba26
Kristofer Rye <kristofer.rye@gmail.com>
2013-06-15 14:13:58 -0400

    add 404 and 500 handle

--- serve.rb
+++ serve.rb
@@ -8,3 +8,11 @@ require 'rubygems'
 get '/' do
 	File.read(File.join('public', 'index.html'))
 end
+
+not_found do
+	File.read(File.join('public', '404.html'))
+end
+
+error 500 do
+	File.read(File.join('public', '500.html'))
+end
