6f311e941a35813c2f112cde850695dbb8783ab4
Kristofer Rye <kristofer.rye@gmail.com>
2013-07-24 10:09:27 -0400

    oops, we need resources, I guess...

---
+++ public/js/overrides.js
@@ -0,0 +1,29 @@
+// // Sticky sidebar w/jQuery
+// sidebarwidth = $(".sidebar-width").css('width');
+// bodypaddingtop = $(".navbar-fixed-top").css('height');
+// // sidebarheight = $("body").css('height');
+// $('.sidebar-nav-fixed').css('width', sidebarwidth);
+// $('.sidebar-nav-fixed').css('height', sidebarheight);
+// $('body').css('paddingTop', bodypaddingtop)
+// contentmargin = parseInt(sidebarwidth)
+// $('.span-fixed-sidebar').css('marginLeft', contentmargin);
+// $('.span-fixed-sidebar').css('paddingLeft', 60);
+function resizeit() {
+	// Sticky sidebar w/jQuery
+	sidebarwidth = $(".sidebar-width").css ('width');
+
+	bodypaddingtop = $(".navbar-fixed-top").css ('height');
+	// sidebarheight = $("body").css('height');
+	$('.sidebar-nav-fixed').css ('width', sidebarwidth);
+	$('.sidebar-nav-fixed').css ('height', sidebarheight);
+	$('body').css ('paddingTop', bodypaddingtop)
+	contentmargin = parseInt (sidebarwidth)
+	$('.span-fixed-sidebar').css ('marginLeft', contentmargin);
+	$('.span-fixed-sidebar').css ('paddingLeft', 60);
+}
+
+$(window).resize(function () {
+	resizeit();
+});
+
+resizeit ();
